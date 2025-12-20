37689e391ffc6243251c7d95963d47c45936b85d
Hồ Quốc Khải <khaiho6040@gmail.com>
2025-12-20 16:10:53 +0700

    Đã xong phần ui của from đăng ký đăng nhập chưa có ux validate hay kiểm tra tài khoả

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,25 @@
-import { useAppDispatch, useAppSelector } from './hooks'
-import { Link, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import { useAppDispatch, useAppSelector } from "./hooks";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-import './App.css'
+import "./App.css";
+import FormAuth from "./ui/FromAuth";
 
 function App() {
-
   return (
     <>
-      
-      <nav style={{ display: 'flex', gap: 12, marginBottom: 16 }}>
+      <nav style={{ display: "flex", gap: 12, marginBottom: 16 }}>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <FormAuth />
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
