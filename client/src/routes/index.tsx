3db5d1e4443e471f726469da22c751fbe0954121
PhatNguyen06 <nguyendaiphat06@gmail.com>
2025-12-22 18:49:12 +0700

    admin fix routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,41 +1,47 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
+
 // Layouts
 import CustomerLayout from "../layouts/CustomerLayout";
 import AdminLayout from "../layouts/AdminLayout";
+
 // Components
 import ProtectedRoute from "./ProtectedRoute";
+
 // Pages
 import HomePage from "../pages/user/homePage";
-import DashboardPage from "../pages/admin/DashboardPage";
-import AboutPage from "../pages/user/AboutPage";
 import BookingTicket from "../pages/user/BookingTicket";
+import AboutPage from "../pages/user/AboutPage";
+import DashboardPage from "../pages/admin/DashboardPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
+  {
+    path: "/",
+    element: <CustomerLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "bookingTicket", element: <BookingTicket /> },
+    ],
+  },
+
+  {
+    path: "/about-page",
+    element: <CustomerLayout />,
+    children: [{ index: true, element: <AboutPage /> }],
+  },
+
+  {
+    path: "/customer",
+    element: <ProtectedRoute requiredRole="CUSTOMER" />,
+    children: [
+      {
         element: <CustomerLayout />,
         children: [
-            {
-                index: true,
-                element: <HomePage />,
-            },
-        ],
-    },
-    {
-        path: "/customer",
-        element: <ProtectedRoute requiredRole="CUSTOMER" />,
-        children: [
-            {
-                element: <CustomerLayout />,
-                children: [
-          {
-            path: "profile",
-            element: <div>Customer Profile Placeholder</div>,
-          },
+          { path: "profile", element: <div>Customer Profile Placeholder</div> },
         ],
       },
     ],
   },
+
   {
     path: "/admin",
     element: <ProtectedRoute requiredRole="ADMIN" />,
@@ -43,30 +49,12 @@ export const router = createBrowserRouter([
       {
         element: <AdminLayout />,
         children: [
-          {
-            index: true,
-            element: <Navigate to="dashboard" replace />,
-          },
-          {
-            path: "dashboard",
-            element: <DashboardPage />,
-          },
+          { index: true, element: <Navigate to="dashboard" replace /> },
+          { path: "dashboard", element: <DashboardPage /> },
         ],
       },
     ],
   },
-  {
-    path: "/about-page",
-    element: <CustomerLayout />,
-    children: [
-      {
-        index: true,
-        element: <AboutPage />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <Navigate to="/" replace />,
-  },
+
+  { path: "*", element: <Navigate to="/" replace /> },
 ]);
